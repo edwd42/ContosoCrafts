8826f65ab0769b5c934277752324f70f399c6039
edward.dupre <edward.dupre@redcross.org>
2021-08-09 20:43:03 -0400

    Enhance Web API with [HttpGet] in ProductsController.cs

--- ContosoCrafts.WebSite/Controllers/ProductsController.cs
+++ ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -29,5 +29,13 @@ namespace ContosoCrafts.WebSite.Controllers
         {
             return ProductService.GetProducts();
         }
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string ProductId, [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
     }
 }
